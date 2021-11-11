b6e04b2d5c60b7ce0432606037bccd95fd23fb2f
emilyann211 <93425618+emilyann211@users.noreply.github.com>
2021-11-11 07:04:10 -0600

    Initialize split-terminal-composition

---
+++ packages/terminal/src/browser/base/terminal-client.ts
@@ -0,0 +1,98 @@
+/********************************************************************************
+ * Copyright (C) 2019 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+
+import { IShellTerminalServerOptions } from '../../common/shell-terminal-protocol';
+import { Disposable } from '@theia/core';
+import { TerminalWidget, TerminalSize } from './terminal-widget';
+
+export type TerminalClientFactory = (options: TerminalClientOptions, terminalWidget: TerminalWidget) => TerminalClient;
+export const TerminalClientFactory = Symbol('TerminalClientFactory');
+
+/**
+ * TerminalClient contains connection logic between terminal server side and terminal widget.
+ * TerminalClient is responsible:
+ * - create new terminal process;
+ * - attach to the already running process on the backend(in case if backend support it);
+ * - create connection with backend process;
+ * - send output to the terminal widget from backend;
+ * - send user input from terminal widget to the backend.
+ * - kill backend process(in case if backend support it);
+ * - resize backend output frame(in case if backend support it);
+ */
+export const TerminalClient = Symbol('TerminalClient');
+export interface TerminalClient extends Disposable {
+
+    /**
+     * Terminal client options to setup backend terminal process
+     * and control terminal client configuration.
+     */
+    readonly options: TerminalClientOptions;
+
+    /**
+     * Terminal widget which is under TerminalClient control.
+     */
+    readonly widget: TerminalWidget;
+
+    /**
+     * Return unique backend terminal process id(Notice: it's not a PID, it's backend id to count running processes).
+     */
+    readonly terminalId: number;
+
+    /**
+     * Return unique process id(PID).
+     */
+    readonly processId: Promise<number>
+
+    /**
+     * Create new process and attach terminal widget to this process.
+     */
+    createAndAttach(): Promise<number>;
+
+    /**
+     * Attach to already running process.
+     * @param terminalId - unique process backend id(it's not PID, it's backend id to count runnting processes).
+     * @param createNewTerminalOnFaill - spawn new process in case if
+     * target process was not found by terminalId or it's gone. NOTICE: False by default.
+     */
+    attach(terminalId: number, createNewTerminalOnFail?: boolean): Promise<number>;
+
+    /**
+     * Resize terminal process on the backend side.
+     * @param cols - amount displayed columns in the output frame.
+     * @param rows - amount displayed rows in the output frame.
+     */
+    resize(size: TerminalSize): Promise<void>;
+
+    /**
+     * Kill backend process.
+     */
+    kill(): Promise<void>;
+
+    /**
+     * Send input to the backend process.
+     * @param text - content to send.
+     */
+    sendText(text: string): Promise<void>;
+}
+
+/**
+ * Terminal client options.
+ */
+export const TerminalClientOptions = Symbol('TerminalClientOptions');
+export interface TerminalClientOptions extends Partial<IShellTerminalServerOptions> {
+    // Kill backend process in case if user closed TerminalWidget manually. NOTICE: False by default.
+    readonly closeOnDispose?: boolean;
+}
---
+++ packages/terminal/src/browser/base/terminal-widget.ts
@@ -0,0 +1,158 @@
+/********************************************************************************
+ * Copyright (C) 2018 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+
+import { Event } from '@theia/core';
+import { BaseWidget } from '@theia/core/lib/browser';
+
+/*
+ * Object to store terminal widget size.
+ */
+export class TerminalSize {
+    readonly cols: number;
+    readonly rows: number;
+}
+
+/**
+ * Terminal UI widget.
+ * Terminal widget responsible to render backend output and catch user input.
+ */
+export abstract class TerminalWidget extends BaseWidget {
+
+    /**
+     * Return current terminal size.
+     */
+    abstract readonly size: TerminalSize;
+
+    /**
+     * Return unique process id(PID).
+     */
+    abstract processId: Promise<number>;
+
+    /**
+     * Start terminal and return terminal id.
+     * @deprecated use createAndAttach() and attach() instead.
+     * @param id - terminal id.
+     */
+    abstract start(id?: number): Promise<number>;
+
+    /**
+     * Create new process and attach terminal widget to this process.
+     */
+    abstract createAndAttach(): Promise<number>;
+
+    /**
+     * Attach to already running process.
+     * @param terminalId - unique process backend id(it's not PID, it's backend id to count runnting processes).
+     * @param createNewTerminalOnFaill - spawn new process in case if
+     * target process was not found by terminalId or it's gone. NOTICE: False by default.
+     */
+    abstract attach(processId: number, createNewProcessOnFail?: boolean): Promise<number>;
+
+    /**
+     * Send text to the terminal server.
+     * @param text - text content.
+     */
+    abstract sendText(text: string): void;
+
+    /**
+     * Write some text content to the widget.
+     */
+    abstract write(data: string): void;
+
+    /*
+     * Send event on open terminal widget.
+     */
+    abstract onDidOpen: Event<void>;
+
+    /*
+     * Send event on user text typing.
+     */
+    abstract onUserInput: Event<string | undefined>;
+
+    /*
+     * Send event on terminal resize.
+     */
+    abstract onTerminalResize: Event<TerminalSize>;
+
+    /**
+     * Event which fires when terminal did closed. Event value contains closed terminal widget definition.
+     */
+    abstract onTerminalDidClose: Event<TerminalWidget>;
+
+    /**
+     * Cleat terminal output.
+     */
+    abstract clearOutput(): void;
+
+    /**
+     * Reset terminal to the initial state. Could be useful for reconnection.
+     */
+    abstract reset(): void;
+}
+
+/**
+ * Terminal widget options.
+ */
+export const TerminalWidgetOptions = Symbol('TerminalWidgetOptions');
+export interface TerminalWidgetOptions {
+
+    /**
+     * Human readable terminal representation on the UI.
+     */
+    readonly title?: string;
+
+    /**
+     * Path to the executable shell. For example: `/bin/bash`, `bash`, `sh`.
+     */
+    readonly shellPath?: string;
+
+    /**
+     * Shell arguments to executable shell, for example: [`-l`] - without login.
+     */
+    readonly shellArgs?: string[];
+
+    /**
+     * Current working directory.
+     */
+    readonly cwd?: string;
+
+    /**
+     * Environment variables for terminal.
+     */
+    readonly env?: { [key: string]: string | null };
+
+    /**
+     * In case `destroyTermOnClose` is true - terminal process will be destroyed on close terminal widget, otherwise will be kept
+     * alive.
+     */
+    readonly destroyTermOnClose?: boolean;
+
+    /**
+     * Terminal server side can send to the client `terminal title` to display this value on the UI. If
+     * useServerTitle = true then display this title, otherwise display title defined by 'title' argument.
+     */
+    readonly useServerTitle?: boolean;
+
+    /**
+     * Terminal id. Should be unique for all DOM.
+     */
+    readonly id?: string;
+
+    /**
+     * Terminal attributes. Can be useful to apply some implementation specific information.
+     */
+    readonly attributes?: { [key: string]: string | null };
+}
