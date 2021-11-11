f67dc4c6f341b289d230272e6a3b1bcb26770a54
emilyann211 <93425618+emilyann211@users.noreply.github.com>
2021-11-11 07:05:27 -0600

    Initialize useAddressDep

---
+++ packages/plugin-ext/src/plugin/node/env-node-ext.ts
@@ -0,0 +1,60 @@
+/********************************************************************************
+ * Copyright (C) 2019 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+
+import { EnvExtImpl } from '../env';
+import { RPCProtocol } from '../../common/rpc-protocol';
+import { createHash } from 'crypto';
+import { mac } from 'address';
+import { v4 } from 'uuid';
+
+/**
+ * Provides machineId using mac address. It's only possible on node side
+ * Extending the common class
+ */
+export class EnvNodeExtImpl extends EnvExtImpl {
+
+    private macMachineId: string;
+
+    constructor(rpc: RPCProtocol) {
+        super(rpc);
+        console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Let generate unique macMachineId!');
+        mac((err: Error, macAddress: string) => {
+            if (err) {
+                console.log('>>>>>>>>Unable to get address...');
+                this.macMachineId = v4();
+            } else {
+                console.log('So address!!!!', macAddress);
+                this.macMachineId = createHash('sha256').update(macAddress, 'utf8').digest('hex');
+            }
+        });
+
+    }
+
+    /**
+     * override machineID
+     */
+    get machineId(): string {
+        return this.macMachineId;
+    }
+
+    /**
+     * Provides application root.
+     */
+    get appRoot(): string {
+        return __dirname;
+    }
+
+}
