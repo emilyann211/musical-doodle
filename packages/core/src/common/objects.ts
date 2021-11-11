1432be2a99146428c6e8a40d181fca589f367aaf
emilyann211 <93425618+emilyann211@users.noreply.github.com>
2021-11-11 07:02:48 -0600

    Initialize plugin-ext-open

---
+++ packages/core/src/common/objects.ts
@@ -0,0 +1,48 @@
+/********************************************************************************
+ * Copyright (C) 2018 Ericsson and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+
+export function deepFreeze<T>(obj: T): T {
+    if (!obj || typeof obj !== 'object') {
+        return obj;
+    }
+    const stack: any[] = [obj];
+    while (stack.length > 0) {
+        const objectToFreeze = stack.shift();
+        Object.freeze(objectToFreeze);
+        for (const key in objectToFreeze) {
+            if (_hasOwnProperty.call(objectToFreeze, key)) {
+                const prop = objectToFreeze[key];
+                if (typeof prop === 'object' && !Object.isFrozen(prop)) {
+                    stack.push(prop);
+                }
+            }
+        }
+    }
+    return obj;
+}
+
+const _hasOwnProperty = Object.prototype.hasOwnProperty;
+
+export function notEmpty<T>(arg: T | undefined | null): arg is T {
+    return arg !== undefined && arg !== null;
+}
+
+/**
+ * `true` if the argument is an empty object. Otherwise, `false`.
+ */
+export function isEmpty(arg: Object): boolean {
+    return Object.keys(arg).length === 0 && arg.constructor === Object;
+}
